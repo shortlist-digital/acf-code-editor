41d5a5c87b9fbdace79345c4866c2213910943fe
Jon Sherrard <jon@andthats.it>
2016-06-17 10:00:01 +0100

    initial js-work

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+
+import 'codemirror/lib/codemirror'
+
+console.log(CodeMirror)
