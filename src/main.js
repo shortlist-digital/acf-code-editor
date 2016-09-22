10b3410e789e285adf2d775797df08593141569f
Jon Sherrard <jon@andthats.it>
2016-09-22 11:27:26 +0100

    add on change event to refresh editor

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/jsx/jsx'
+import 'codemirror/mode/twig/twig'
+import 'codemirror/mode/php/php'
 import 'codemirror/mode/css/css'
 import 'codemirror/mode/htmlmixed/htmlmixed'
 
@@ -9,10 +12,12 @@ window.$ = window.$ || jQuery || window.jQuery
 
 let sw_interval
 
+let editorInstances = []
+
 let instantiator = function(el) {
 	let instance = CodeMirror.fromTextArea(el, {
     lineNumbers: true,
-    mode: "htmlmixed",
+    mode: 'htmlmixed',
 		theme: 'monokai',
 		tabSize: 2,
 		lineWrapping: true
@@ -22,20 +27,28 @@ let instantiator = function(el) {
 
 const strict_wysiwyg = function() {
 	console.log('Interval fired')
-  if ((typeof $ !== "undefined" && $ !== null)) {
+  if ((typeof $ !== 'undefined' && $ !== null)) {
     clearInterval(sw_interval)
     var editors = $('.codemirror-wrapper textarea')
     if (editors.length) {
       for (var i=0;i < editors.length; i++) {
-        instantiator(editors[i])
+        editorInstances.push(instantiator(editors[i]))
       }
     }
   }
 }
 
 acf.add_action('ready', function() {
-  sw_interval = setInterval(strict_wysiwyg, 10);
-});
+  sw_interval = setInterval(strict_wysiwyg, 10)
+})
+
+acf.add_action('show_field', function( $field, context ){
+  if (editorInstances.length) {
+    for (let i=0;i < editorInstances.length; i++) {
+      editorInstances[i].refresh()
+    }
+  }
+})
 
 acf.add_action('append', ( $el ) => {
   // $el will be equivalent to the new element being appended $('tr.row')
@@ -44,5 +57,6 @@ acf.add_action('append', ( $el ) => {
   if ($field.length) {
     let newInstance = instantiator($field[0])
 		newInstance.focus()
+    editorInstances.push(newInstace)
   }
 })
